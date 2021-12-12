9f00c51c4392ec90b6cf57e875156ca71a80c728
Shija Kpamsar Daniel <86082029+KPAMsar@users.noreply.github.com>
2021-12-12 21:04:25 +0100

    Create ass.py

---
+++ ass.py
@@ -0,0 +1,4 @@
+print("Hello how are you doing??\n")
+name = input("can I please know your name?\n")
+age = input("can I also know your age ?\n")
+print(f"so you are {name} and {age} years old. Nice to meet you")
